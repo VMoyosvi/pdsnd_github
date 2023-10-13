98410e7c57da9ca460f95eaa83651ead8b34b395
Victor Moyosvi <vmoyosvi@gmail.com>
2023-10-13 22:46:56 +0200

    Modify the City dictionary name

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
+CITY_DICT = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 #define a list of months to use for input and and getting the number of the month
